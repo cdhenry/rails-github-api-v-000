4f77f2fe0e4f504a729b1631be4ea337f11241e4
Chris Henry <cdhenry@gmail.com>
2018-05-01 19:30:39 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,9 @@
 class SessionsController < ApplicationController
   def create
-    resp = Faraday.get("https://github.com/oauth/access_token") do |req|
+    resp = Faraday.get("https://github.com/login/oauth/access_token") do |req|
       req.params['client_id'] = ENV['GITHUB_ID']
       req.params['client_secret'] = ENV['GITHUB_SECRET']
-      req.params['redirect_uri'] = "http://localhost:3000/auth"
+      req.params['redirect_uri'] = "http://67.205.188.72:38790/auth"
       req.params['code'] = params[:code]
     end
 
