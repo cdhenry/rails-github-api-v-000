f68c6f8ba58a470d9ca6d10250739c9a3006e260
Chris Henry <cdhenry@gmail.com>
2018-05-01 20:58:32 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -1,7 +1,7 @@
 class RepositoriesController < ApplicationController
   def index
     response = Faraday.get "https://api.github.com/user/repos", {}, {
-      'Authorization' => "token #{session[:token]}", 
+      'Authorization' => "token #{session[:token]}",
       'Accept' => 'application/json'
     }
     @repos_array = JSON.parse(response.body)
@@ -11,9 +11,9 @@ class RepositoriesController < ApplicationController
     response = Faraday.post "https://api.github.com/user/repos", {
       name: params[:name]
     }.to_json, {
-        'Authorization' => "token #{session[:token]}", 
+        'Authorization' => "token #{session[:token]}",
         'Accept' => 'application/json'
       }
-    redirect_to '/'
+    redirect_to root_path
   end
 end
