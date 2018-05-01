4389c0a3aef8360f300b4aa8062502762e11b078
Chris Henry <cdhenry@gmail.com>
2018-05-01 19:55:27 +0000

    automatically backed up by learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,6 @@ class SessionsController < ApplicationController
       req.params['redirect_uri'] = "http://67.205.188.72:38790/auth"
       req.params['code'] = params[:code]
     end
-    binding.pry
     start = body.index("=")
     finish = body.index("&")
     session[:token] = resp.body[start..finish]
