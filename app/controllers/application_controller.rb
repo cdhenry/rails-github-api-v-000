8f2aed1fe3d0d7eaa77d8414e101e145de388538
Chris Henry <cdhenry@gmail.com>
2018-05-01 19:25:25 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     def authenticate_user
       # make sure to pass in the scope parameter (`repo` scope should be appropriate for what we want to do) in step of the auth process!
       # https://developer.github.com/apps/building-oauth-apps/authorization-options-for-oauth-apps/#web-application-flow
+      client_id = ENV['GITHUB_ID']
+      redirect_uri = CGI.escape("http://67.205.188.72:38790/auth")
+      foursquare_url = "https://foursquare.com/oauth2/authenticate?client_id=#{client_id}&response_type=code&redirect_uri=#{redirect_uri}"
+      redirect_to foursquare_url unless logged_in?
     end
 
     def logged_in?
