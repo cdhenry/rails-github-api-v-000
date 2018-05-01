dac6041df122c32c2e8f94afba25ca5b6a6372e4
Chris Henry <cdhenry@gmail.com>
2018-05-01 19:37:37 +0000

    automatically backed up by learn

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -2,13 +2,12 @@ class RepositoriesController < ApplicationController
   def index
     resp = Faraday.get("https://api.github.com/user/repos") do |req|
       req.params['oauth_token'] = session[:token]
-      # don't forget that pesky v param for versioning
-      req.params['v'] = '20160201'
     end
-    @friends = JSON.parse(resp.body)["response"]["friends"]["items"]
+    binding.pry
+    @repos = JSON.parse(resp.body)
   end
 
   def create
-    
+
   end
 end
